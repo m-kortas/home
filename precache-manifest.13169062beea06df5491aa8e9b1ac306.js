5bbb466eb494cdae9fc4f7ab4499ba9c6037e74d
m-kortas <mac@Magda-Home.local>
2022-03-22 20:38:52 +0100

    Updates

---
+++ precache-manifest.13169062beea06df5491aa8e9b1ac306.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c1dff3f7d1045b36aa6d6c681bdf3d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "f95ae42328b36977a59a",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f95ae42328b36977a59a",
+    "url": "/home/static/js/main.73b8bc39.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "ee44e42f15fcdb7b0b021420eb16d81b",
+    "url": "/home/static/media/resume.ee44e42f.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.13169062beea06df5491aa8e9b1ac306.js"
 );
 
 self.addEventListener('message', (event) => {
