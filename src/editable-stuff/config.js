9ffd93bf63a74901f109cb7221b36270633c8f31
m-kortas <mac@Magda-Home.local>
2022-03-22 21:18:54 +0100

    updating files

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -1,65 +1,43 @@
-// Navigation Bar SECTION
 const navBar = {
   show: true,
 };
 
-// Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Magdalena",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Kortas",
+  message: "  Passionate about and specialised in building AI for Good solutions. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/m-kortas",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "",
     }
   ],
 };
 
-// ABOUT SECTION
-// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
-//a) your Instagram username
-//      i.e:profilePictureLink:"johnDoe123",
-//b) a link to an hosted image
-//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
-//c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
-//d) If you do not want any picture to be displayed, just leave it empty :)
-//      i.e: profilePictureLink: "",
-// For Resume either provide link to your resume or import from "editable-stuff" directory
-//     i.e resume: require("../editable-stuff/resume.pdf"),
-//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
-
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
-  imageSize: 375,
+  imageLink: require("../editable-stuff/magda.jpg"),
+  imageSize: 200,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "I am an ambitious, internationally recognised and certified Data Science expert with​ 4 years of professional experience in Data Science & Machine Learning gained in both the private and civic sector, working on international projects in the fields of Finance, Energy, Health, Circular Economy, Infrastructure, Travel, Transport and Education. Passionate about and specialised in building AI for Good solutions. An active supporter and promoter of women and gender minorities in Data Science and Machine Learning, lead and participated in many international AI4Good projects.", 
 };
 
-// PROJECTS SECTION
-// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
-//      i.e: reposLength: 0,
-// If you want to display specfic projects, add the repository names,
-//      i.e ["repository-1", "repo-2"]
+
 const repos = {
   show: true,
-  heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
-  specificRepos: [],
+  heading: "Some of my projects",
+  gitHubUsername: "m-kortas", 
+  reposLength: 0,
+  specificRepos: ['Sound-based-bird-species-detection', 'detect-waste', 'AI-playground', 'anomaly-detection'],
 };
 
-// Leadership SECTION
 const leadership = {
   show: false,
   heading: "Leadership",
@@ -67,12 +45,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../editable-stuff/magda.jpg"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
+      img: require("../editable-stuff/magda.jpg"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
@@ -83,9 +61,8 @@ const leadership = {
   }
 };
 
-// SKILLS SECTION
 const skills = {
-  show: true,
+  show: false,
   heading: "Skills",
   hardSkills: [
     { name: "Python", value: 90 },
@@ -109,35 +86,45 @@ const skills = {
   ],
 };
 
-// GET IN TOUCH SECTION
 const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I’m looking to collaborate on AI for Good projects (especially climate change counteraction)! If you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "magdalenekortas@gmail.com",
 };
 
 const experiences = {
-  show: false,
+  show: true,
   heading: "Experiences",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
+      role: 'Data Scientist - Carl Data Solutions',
+      companylogo: require('../assets/img/carl.jpg'),
       date: 'June 2018 – Present',
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
+      role: 'Data Scientist - IHS Market (S&P)',
+      companylogo: require('../assets/img/ihs.jpg'),
+      date: 'May 2017 – May 2018',
+    },
+    {
+      role: 'Data Scientist - Magdalena Kortas Data Solutions',
+      companylogo: require('../assets/img/magda_c.jpg'),
+      date: 'May 2017 – May 2018',
+    },
+    {
+      role: 'Data Scientist - PredictX',
+      companylogo: require('../assets/img/predictx.jpg'),
+      date: 'May 2017 – May 2018',
+    },
+    {
+      role: 'Data Scientist - Nordea',
+      companylogo: require('../assets/img/nordea.jpg'),
       date: 'May 2017 – May 2018',
     },
   ]
 }
 
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
 
 export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
